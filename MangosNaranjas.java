c92a774674daab6fb8a97f3e2f7c868f76ee3bf6
LuisEscamilla9708 <127147479+LuisEscamilla9708@users.noreply.github.com>
2025-07-11 11:42:57 -0600

    Actualizacion de codigo clase MangosNaranjas

--- MangosNaranjas.java
+++ MangosNaranjas.java
@@ -1,1 +1,27 @@
+package mangos;
+
+public class MangosNaranjas {
+    private int mangos;
+    private int naranjas;
+    public MangosNaranjas(int mangos, int naranjas) {
+        this.mangos = mangos;
+        this.naranjas = naranjas;
+    }
+    private int calculo(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public void imprimir() {
+        int cajas = calculo(mangos, naranjas);
+        int mangosPorCaja = mangos / cajas;
+        int naranjasPorCaja = naranjas / cajas;
+        System.out.println("El numero de cajas es: " + cajas);
+        System.out.println("El numero de mangos en una caja es: " + mangosPorCaja);
+        System.out.println("El numero de naranjas en una caja es: " + naranjasPorCaja);
+    }
+}
 
