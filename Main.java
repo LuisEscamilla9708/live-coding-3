40fec45be4d534ddac508ab36fd3bb5d8476ef5b
LuisEscamilla9708 <127147479+LuisEscamilla9708@users.noreply.github.com>
2025-07-11 11:41:15 -0600

    cambios en main

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+package mangos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("introduce el numero de mangos:");
+        int mangos = scanner.nextInt();
+        System.out.print("introduce el numero de naranja:");
+        int naranjas = scanner.nextInt();
+
+        MangosNaranjas cajas = new MangosNaranjas(mangos, naranjas);
+        cajas.imprimir();
+
+        scanner.close();
+    }
+}
 
